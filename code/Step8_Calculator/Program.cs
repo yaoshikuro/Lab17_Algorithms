9b25014679dffb738d12e98a74cfab6d0666e3f7
yaoshikuro <yukivoron@gmail.com>
2026-04-14 15:01:53 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+Console.WriteLine("Калькулятор");
+Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите операцию которую хотите сделать: ");
+string op = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (op == "+")
+{
+    Console.WriteLine($"Сумма: {Sum(x, y)}");
+}
+else if (op == "-")
+{
+    Console.WriteLine($"Разность: {Subtract(x, y)}");
+}
+else if (op == "*")
+{
+    Console.WriteLine($"Произведение: {Multiply(x, y)}");
+}
+else if (op == "/")
+{
+    Console.WriteLine($"Частное: {Divide(x, y):F2}");
+}
+else
+{
+    Console.WriteLine($"пиши правильно болван");
+}
